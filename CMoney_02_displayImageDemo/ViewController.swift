e9505e6d6b93c01e8615f6fb6163eba5dc69f648
張彩虹 <ellapc@zhangcaihongde-macbook-pro.local>
2020-08-06 22:30:49 +0800

    Initial Commit

---
+++ CMoney_02_displayImageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CMoney_02_displayImageDemo
+//
+//  Created by 張彩虹 on 2020/8/6.
+//  Copyright © 2020年 張彩虹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CMoney_02_displayImageDemoTests/CMoney_02_displayImageDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  CMoney_02_displayImageDemoTests.swift
+//  CMoney_02_displayImageDemoTests
+//
+//  Created by 張彩虹 on 2020/8/6.
+//  Copyright © 2020年 張彩虹. All rights reserved.
+//
+
+import XCTest
+@testable import CMoney_02_displayImageDemo
+
+class CMoney_02_displayImageDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
